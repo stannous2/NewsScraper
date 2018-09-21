a512888f56c42aa7e200d18c58870b787d355c8f
stannous2 <stannous2@yahoo.com>
2018-09-20 23:46:46 -0700

    updated server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -26,8 +26,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// // Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/MongoScraper");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/MongoScrape";
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/MongoScraper");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
